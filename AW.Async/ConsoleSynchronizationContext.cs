2c2cdf004d12abf3b7aac9c3458e606be72dcd26
Joshua Rodgers <bytemr@gmail.com>
2012-01-01 23:48:18 -0700

    Added AsyncUtility.cs, this class provides several methods and properties used for asynchronous console applications. Added a custom SynchronizationContext and TaskScheduler for use with console applications.
    Work Items: #3511, #3510

---
+++ AW.Async/ConsoleSynchronizationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace AW.Async
+{
+    sealed class ConsoleSynchronizationContext : SynchronizationContext
+    {
+        readonly ConcurrentQueue<Action> _messagesToProcess = new ConcurrentQueue<Action>();
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _messagesToProcess.Enqueue(() => d(state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            _messagesToProcess.Enqueue(() => d(state));
+        }
+
+        public void Dispatch()
+        {
+            Action action;
+            var result = _messagesToProcess.TryDequeue(out action);
+
+            if (result)
+            {
+                action();
+            }
+        }
+    }
+}
